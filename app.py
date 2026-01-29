49f27d64b2a108b8fbb7ddf30ad15f2a6a377f9a
Mamadou Douah Barry <douahbarry123@gmail.com>
2026-01-29 00:59:36 +0000

    modif

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_cors import CORS
 from routes.clean_route import clean_bp
 from routes.download_route import download_bp
+import os
 
 
 app = Flask(__name__)
 CORS(app)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.register_blueprint(clean_bp)
 app.register_blueprint(download_bp)
 @app.route("/")
